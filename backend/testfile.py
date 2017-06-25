a78698ea2d6318356ea6944a3710df073b0c9928
spiegel421 <spiegel421@gmail.com>
2017-06-25 12:22:25 -0400

    Update testfile.py

--- backend/testfile.py
+++ backend/testfile.py
@@ -3,5 +3,6 @@ from database import update_album_tags, update_album_ratings, update_song_rating
 update_album_tags('1', '2', '3')
 update_album_tags('1', '2', '3')
 update_album_ratings('1', '2', 6)
+update_album_ratings('1', '2', 5)
 update_song_ratings('1', '4', 3)
 read_album_tags()

2114b7f604eb12a48e9eb840a9f04c8df86924d9
spiegel421 <spiegel421@gmail.com>
2017-06-25 19:40:09 -0400

    Update testfile.py

--- backend/testfile.py
+++ backend/testfile.py
@@ -2,7 +2,7 @@ from database import update_album_tags, update_album_ratings, update_song_rating
 from tagmatrix import convert_to_matrix, convert_to_npmi, autoencode
 from random import randrange
 
-for i in range(10000):
+for i in range(1000):
   update_album_tags(str(randrange(100)), str(randrange(100)), str(randrange(100)))
 matrix = convert_to_matrix(read_album_tags())
 npmi_matrix = convert_to_npmi(matrix)

152b39449716219187b28e0518509bc39d6f2f63
spiegel421 <spiegel421@gmail.com>
2017-06-25 02:09:39 -0400

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -80,7 +80,7 @@ def update_album_tags(user, album, tag):
   data_album_tag = (user, album, tag)
   
   cursor.execute(check_exists, data_album_tag)
-  if cursor[0][0] == 0:
+  if cursor == 0:
     cursor.execute(add_album_tag, data_album_tag)
   cnx.commit()
   

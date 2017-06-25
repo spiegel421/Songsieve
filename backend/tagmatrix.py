edaa8e9e772046a041c9c5c7b5176db189857eca
spiegel421 <spiegel421@gmail.com>
2017-06-25 15:18:42 -0400

    Update tagmatrix.py

--- backend/tagmatrix.py
+++ backend/tagmatrix.py
@@ -1,1 +1,6 @@
+# Establishes, in binary fashion, whether each album has each tag based on PMI values and auto-encoding.
+import numpy as np
+import pandas as pd
 
+def convert_to_matrix(album_tag_dict):
+  return pd.DataFrame(album_tag_dict).T.fillna(0)

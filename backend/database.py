9ef460c560aeb3b061ef8392fc0072fa3b27907e
spiegel421 <spiegel421@gmail.com>
2017-06-25 01:56:27 -0400

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -69,7 +69,7 @@ def update_album_tags(user, album, tag):
   cursor = cnx.cursor()
   cnx.database = DB_NAME
   
-  add_album_tag = ("REPLACE INTO album_tags "
+  add_album_tag = ("INSERT IGNORE INTO album_tags "
              "VALUES (%s, %s, %s); ")
   
   data_album_tag = (user, album, tag)

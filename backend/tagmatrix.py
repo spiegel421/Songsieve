10ace4eeb0fcbe396decb82bfe6031dfa9347d4a
spiegel421 <spiegel421@gmail.com>
2017-06-25 20:46:22 -0400

    Update tagmatrix.py

--- backend/tagmatrix.py
+++ backend/tagmatrix.py
@@ -66,6 +66,6 @@ def find_distance_matrix(count_matrix, encoded_space):
     print len(y)
     y = [1 if item > 0 else 0 for item in y.values[0]]
     clf.fit(encoded_space.values, y)
-    distance_matrix[tag] = clf.decision_function(encoded_space.values)
+    distance_matrix.loc[[tag]] = clf.decision_function(encoded_space.values)
     
   return distance_matrix

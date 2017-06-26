8be610dfb2c78c63c552c94853eed878a3de2fec
spiegel421 <spiegel421@gmail.com>
2017-06-26 00:45:04 -0400

    Update testfile.py

--- backend/testfile.py
+++ backend/testfile.py
@@ -5,7 +5,9 @@ from random import randrange
 #for i in range(1000):
 #  update_album_tags(str(randrange(100)), "A" + str(randrange(20)), "T" + str(randrange(100)))
 matrix = convert_to_matrix(read_album_tags())
-npmi_matrix = convert_to_npmi(matrix)
-encoded_space = autoencode(npmi_matrix)
+ppmi_matrix = convert_to_ppmi(matrix)
+encoded_space = autoencode(ppmi_matrix)
 distance_matrix = find_distance_matrix(matrix, encoded_space)
-print rank_distance_matrix(distance_matrix)
+ranked_matrix = rank_distance_matrix(distance_matrix)
+ndcg_values = find_ndcg_values(ppmi_matrix, ranked_matrix)
+print ndcg_values

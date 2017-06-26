1ae830fdeff41c542971f07214009f164059374a
spiegel421 <spiegel421@gmail.com>
2017-06-26 00:07:37 -0400

    Update testfile.py

--- backend/testfile.py
+++ backend/testfile.py
@@ -2,8 +2,8 @@ from database import update_album_tags, update_album_ratings, update_song_rating
 from tagmatrix import convert_to_matrix, convert_to_npmi, autoencode, find_distance_matrix
 from random import randrange
 
-for i in range(1000):
-  update_album_tags(str(randrange(100)), "A" + str(randrange(20)), "T" + str(randrange(100)))
+#for i in range(1000):
+#  update_album_tags(str(randrange(100)), "A" + str(randrange(20)), "T" + str(randrange(100)))
 matrix = convert_to_matrix(read_album_tags())
 npmi_matrix = convert_to_npmi(matrix)
 encoded_space = autoencode(npmi_matrix)

d5cf889b870a14cf20ef810f56834c1041d1fb6c
spiegel421 <spiegel421@gmail.com>
2017-06-25 20:30:23 -0400

    Update tagmatrix.py

--- backend/tagmatrix.py
+++ backend/tagmatrix.py
@@ -62,7 +62,7 @@ def find_distance_matrix(count_matrix, encoded_space):
   
   clf = svm.LinearSVC()
   for tag in count_matrix.columns.values:
-    y = copy.copy(count_matrix).T[tag]
+    y = copy.copy(count_matrix)[:tag]
     print len(y)
     y = [item / item if item > 0 else 0 for item in y.values[0]]
     clf.fit(encoded_space.values, y)

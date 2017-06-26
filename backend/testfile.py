95bd3669f1c6dccf75a6ee56c4942e209595ba30
spiegel421 <spiegel421@gmail.com>
2017-06-25 20:03:50 -0400

    Update testfile.py

--- backend/testfile.py
+++ backend/testfile.py
@@ -3,7 +3,7 @@ from tagmatrix import convert_to_matrix, convert_to_npmi, autoencode, find_dista
 from random import randrange
 
 for i in range(1000):
-  update_album_tags(str(randrange(100)), str(randrange(20), "album"), str(randrange(100)))
+  update_album_tags(str(randrange(100)), str(randrange(20), "album"), str(randrange(100), "tag"))
 matrix = convert_to_matrix(read_album_tags())
 npmi_matrix = convert_to_npmi(matrix)
 encoded_space = autoencode(npmi_matrix)

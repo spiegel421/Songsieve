aa72fb8a29c084a816fd964b9d875cba6579e1b2
spiegel421 <spiegel421@gmail.com>
2017-06-29 11:01:00 -0400

    Update tagmatrix.py

--- backend/tagmatrix.py
+++ backend/tagmatrix.py
@@ -101,3 +101,23 @@ def find_ndcg_values(ppmi_matrix, ranked_matrix):
     ndcg_values[tag] = dcg / idcg
     
   return ndcg_values
+
+# Finds binary table based on ranked matrix and tag NDCG values.
+def find_binary_table(ranked_matrix, percentile, ndcg_values, cutoff_ndcg):
+  interpretable_tags = []
+  for tag in ndcg_values:
+    if ndcg_values[tag] >= cutoff_ndcg:
+      interpretable_tags.append(tag)
+  
+  binary_table = []
+  num_albums = len(ranked_matrix.iloc[0])
+  cutoff_rank = (1 - percentile) * num_albums
+  rankings_dict = ranked_matrix.to_dict(orient='index')
+  for tag in interpretable_tags:
+    album_rankings = rankings_dict[tag]
+    for album in album_rankings:
+      if album_rankings[album] <= cutoff_rank:
+        item = (album, tag)
+        binary_table.append(item)
+        
+  return binary_table

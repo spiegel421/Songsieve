2145bdd91f96b67f62b1ca6ea10dada98c6c07d2
spiegel421 <spiegel421@gmail.com>
2017-06-29 11:02:40 -0400

    Update testfile.py

--- backend/testfile.py
+++ backend/testfile.py
@@ -2,8 +2,8 @@ from tags_ratings import *
 from tagmatrix import *
 from random import randrange
 
-#for i in range(1000):
-#  update_album_tags(str(randrange(100)), "A" + str(randrange(20)), "T" + str(randrange(100)))
+for i in range(1000):
+  update_album_tags(str(randrange(100)), "A" + str(randrange(20)), "T" + str(randrange(100)))
 matrix = convert_to_matrix(read_album_tags())
 ppmi_matrix = convert_to_ppmi(matrix)
 encoded_space = autoencode(ppmi_matrix)
